f06464c8aae31fb1572ae4025c3cc604150652ce
konst <konst>
2002-02-01 00:38:24 +0000

    ''

--- src/imlogger.h
+++ src/imlogger.h
@@ -4,6 +4,17 @@
 #include "icqcommon.h"
 #include "imevents.h"
 
+static const char* imstatus2name[imstatus_size] = {
+    _("Offline"),
+    _("Online"),
+    _("Invisible"),
+    _("Free for chat"),
+    _("DND"),
+    _("Occupied"),
+    _("N/A"),
+    _("Away")
+};
+
 class imlogger {
     protected:
 	ofstream f;
