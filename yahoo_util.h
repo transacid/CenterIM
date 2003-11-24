b1c38ccfc829f10026716f10d1db6b79c0e61552
konst <konst>
2003-11-24 09:20:40 +0000

    ""

--- yahoo_util.h
+++ yahoo_util.h
@@ -67,7 +67,7 @@ void y_strfreev(char ** vector);
 int strncasecmp(const char * s1, const char * s2, size_t n);
 int strcasecmp(const char * s1, const char * s2);
 
-char * strdup(const char *s);
+//char * strdup(const char *s);
 
 int snprintf(char *str, size_t size, const char *format, ...);
 int vsnprintf(char *str, size_t size, const char *format, va_list ap);
