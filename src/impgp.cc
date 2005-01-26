ba7943b7ff79bceefd36e8afeb8815a657c10fb3
konst <konst>
2005-01-26 00:51:11 +0000

    ""

--- src/impgp.cc
+++ src/impgp.cc
@@ -56,6 +56,7 @@ vector<string> impgp::getkeys(bool secretonly) {
 
 gpgme_error_t impgp::passphrase_cb(void *hook, const char *uidhint,
 const char *info, int prevbad, int fd) {
+
     if(!prevbad) {
 	if(passphrase[opname].empty())
 	    passphrase[opname] = conf.getourid(opname).additional["pgppass"];
@@ -89,31 +90,26 @@ string impgp::sign(const string &text, const string &keyid, protocolname pname) 
 	gpgme_set_passphrase_cb(ctx, &passphrase_cb, 0);
 	opname = pname;
 
-	if(!(err = gpgme_get_key(ctx, keyid.c_str(), &key, 1))) {
+	if(!gpgme_get_key(ctx, keyid.c_str(), &key, 1)) {
 	    gpgme_signers_clear(ctx);
 	    gpgme_signers_add(ctx, key);
 
-	    if(!(err = gpgme_data_new_from_mem(&in, text.c_str(), text.size(), 0))) {
-		if(!(err = gpgme_data_new(&out))) {
+	    if(!gpgme_data_new_from_mem(&in, text.c_str(), text.size(), 0)) {
+		if(!gpgme_data_new(&out)) {
 		    if(!(err = gpgme_op_sign(ctx, in, out, GPGME_SIG_MODE_DETACH))) {
 			auto_ptr<char> p(gpgme_data_release_and_get_mem(out, &n));
 			r = p.get();
 			strip(r);
 		    } else {
 			gpgme_data_release(out);
-			r = (string) "sign error 3: " + gpgme_strerror(err);
+			r = (string) "sign error: " + gpgme_strerror(err);
 		    }
-		} else {
-		    r = (string) "sign error 2: " + gpgme_strerror(err);
 		}
+
 		gpgme_data_release(in);
-	    } else {
-		r = (string) "sign error 1: " + gpgme_strerror(err);
 	    }
 
 	    gpgme_key_release(key);
-	} else {
-	    r = (string) "cannot get the key: " + gpgme_strerror(err);
 	}
     }
 
