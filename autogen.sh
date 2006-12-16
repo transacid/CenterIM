1bbe250c718be4658e6387d2845888b99f221504
David Riebenbauer <davrieb@sbox.tugraz.at>
2006-12-16 11:49:10 +0100

    fixes some automake related files
    new configure.ac
    rm configure.in
    add an autogen.sh
    
    Patch © 2006 Mark Pustjens <mark@unkie.org>

---
+++ autogen.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+# script shamelessy taken from openbox
+
+sh() {
+  /bin/sh -c "set -x; $*"
+}
+
+sh autopoint --force # for GNU gettext
+sh libtoolize --copy --force --automake
+
+# ok, this is wierd, but apparantly every subdir
+# in the centericq sources has its own configure
+# no idea why. should fix this.
+
+TOP=$PWD
+traverse=`find $PWD -name "configure.[ia][nc]" -print`
+for i in $traverse; do
+	pushd `dirname $i` > /dev/null
+
+	if test "$PWD" = "$TOP"; then
+		sh aclocal -I m4 $ACLOCAL_FLAGS
+	else
+		sh aclocal $ACLOCAL_FLAGS
+	fi
+	headneeded=`grep AM_CONFIG_HEADER configure.ac`
+	if test ! -z "$headneeded"; then autoheader; fi
+	sh autoconf
+	sh automake --add-missing --copy
+
+	popd > /dev/null
+done
+
+echo
+echo You are now ready to run ./configure
+echo enjoy!
