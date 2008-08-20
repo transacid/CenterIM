f7d5769e1dea1056e61e49b9dfb03242fd337086
Matthias Nieuwenhuisen <git@nieuwenhuisen.de>
2008-08-20 21:25:32 +0200

    another small code style correction

--- src/icqface.cc
+++ src/icqface.cc
@@ -2772,7 +2772,7 @@ void icqface::renderchathistory() {
 	    h = histentry();
 	    h.first = events.back()->getdirection();
 	    if(c->receivedAcks.size() > 0 || c->sentAcks.size() > 0)
-		{
+	    {
 	        breakintolines(text, h.second, sizeWArea.x2-sizeWArea.x1-3);
 	        h.second[0] = ((events.back()->getack())?"+":" ") + h.second[0];
 	        for(unsigned int i = 1; i < h.second.size(); ++i)
