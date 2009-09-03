78051282646359f528a10d8dd31d6231a7aeb057
Frank Altpeter <frank@altpeter.de>
2009-09-03 21:15:14 +0200

    Fix fribidi to build on freebsd

--- kkconsui/src/conscommon.cc
+++ kkconsui/src/conscommon.cc
@@ -274,9 +274,9 @@ string makebidi(const string &buf, int lpad) {
 	// really sick thing
 
     base = FRIBIDI_TYPE_N;
-    fribidi_iso8859_8_to_unicode(cbuf, buf.size(), us);
+    fribidi_charset_to_unicode(FRIBIDI_CHAR_SET_ISO8859_8,cbuf, buf.size(), us);
     fribidi_log2vis(us, buf.size(), &base, out_us, 0, 0, 0);
-    fribidi_unicode_to_iso8859_8(out_us, buf.size(), outstring);
+    fribidi_unicode_to_charset(FRIBIDI_CHAR_SET_ISO8859_8, out_us, buf.size(), outstring);
 
     r = outstring;
 
